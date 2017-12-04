3b4af3340bbe596f89be52ef03375ab98bd08d9a
Felipe F. Schwade <felipefaeschwade@hotmail.com>
2017-12-04 19:43:40 -0300

    Criando buscas com diferentes filtros e ordem de exibição.

--- MongoDb/Program.cs
+++ MongoDb/Program.cs
@@ -11,12 +11,70 @@ namespace MongoDb
         public static async Task Main(string[] args)
         {
             var livrosCollection = MongoDBConnector.GetLivrosCollection;
+
             var filtro = new BsonDocument() { { "Autor", "Machado de Assis" } };
+            Console.WriteLine("Livros do Machado De Assis");
             var livros = await livrosCollection.Find(filtro).ToListAsync();
             foreach (var livro in livros)
             {
                 Console.WriteLine(livro.ToJson<Livro>());
             }
+
+            Console.WriteLine();
+
+            var filtroComBuilder = Builders<Livro>.Filter;
+            var condicoes = filtroComBuilder.Eq(p => p.Autor, "Machado de Assis");
+            Console.WriteLine("Livros do Machado De Assis com Filtro");
+            livros = await livrosCollection.Find(condicoes).ToListAsync();
+            foreach (var livro in livros)
+            {
+                Console.WriteLine(livro.ToJson<Livro>());
+            }
+
+            Console.WriteLine();
+
+            filtroComBuilder = Builders<Livro>.Filter;
+            condicoes = filtroComBuilder.Gte(p => p.Paginas, 200);
+            Console.WriteLine("Livros com mais de 200 páginas");
+            livros = await livrosCollection.Find(condicoes).ToListAsync();
+            foreach (var livro in livros)
+            {
+                Console.WriteLine(livro.ToJson<Livro>());
+            }
+
+            Console.WriteLine();
+
+            filtroComBuilder = Builders<Livro>.Filter;
+            condicoes = filtroComBuilder.Gte(p => p.Paginas, 200) & filtroComBuilder.Gte(l => l.Ano, 1999);
+            Console.WriteLine("Livros com mais de 200 páginas e a partir de 1999");
+            livros = await livrosCollection.Find(condicoes).ToListAsync();
+            foreach (var livro in livros)
+            {
+                Console.WriteLine(livro.ToJson<Livro>());
+            }
+
+            Console.WriteLine();
+
+            filtroComBuilder = Builders<Livro>.Filter;
+            condicoes = filtroComBuilder.AnyEq(p => p.Assuntos, "Ficção Científica");
+            Console.WriteLine("Livros de Ficção Científica");
+            livros = await livrosCollection.Find(condicoes).ToListAsync();
+            foreach (var livro in livros)
+            {
+                Console.WriteLine(livro.ToJson<Livro>());
+            }
+
+            Console.WriteLine();
+
+            filtroComBuilder = Builders<Livro>.Filter;
+            condicoes = filtroComBuilder.Gt(p => p.Paginas, 100);
+            Console.WriteLine("Livros Com mais de 100 páginas em ordem alfabética");
+            livros = await livrosCollection.Find(condicoes).SortBy(l => l.Titulo).ToListAsync();
+            foreach (var livro in livros)
+            {
+                Console.WriteLine(livro.ToJson<Livro>());
+            }
+
             Console.ReadKey();
         }
     }
