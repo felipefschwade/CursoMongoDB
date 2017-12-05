60d5551210ef05e98ebb86c9337a7a0198e3d38a
Felipe F. Schwade <felipefaeschwade@hotmail.com>
2017-12-05 13:49:02 -0300

    Realizando a exclusão de um item no MongoDb.

--- MongoDb/Program.cs
+++ MongoDb/Program.cs
@@ -12,63 +12,23 @@ namespace MongoDb
         {
             var livrosCollection = MongoDBConnector.GetLivrosCollection;
 
-            var filtro = Builders<Livro>.Filter.Eq(l => l.Titulo, "Game of Thrones");
-            Console.WriteLine("Alterando o Game of Thrones");
+            var filtro = Builders<Livro>.Filter.Eq(l => l.Autor, "M. De Assis");
+            Console.WriteLine("Excluido os Livros de M. Assis");
             var livros = await livrosCollection.Find(filtro).ToListAsync();
             foreach (var livro in livros)
             {
                 Console.WriteLine(livro.ToJson<Livro>());
-                livro.Ano = livro.Ano + 1;
-                livro.Paginas = livro.Paginas + 1;
                 await livrosCollection.ReplaceOneAsync(filtro, livro);
             }
 
-            filtro = Builders<Livro>.Filter.Eq(l => l.Titulo, "Game of Thrones");
-            Console.WriteLine("Game of Thrones Alterado");
+            await livrosCollection.DeleteManyAsync(filtro);
+
             livros = await livrosCollection.Find(filtro).ToListAsync();
             foreach (var livro in livros)
             {
                 Console.WriteLine(livro.ToJson<Livro>());
+                await livrosCollection.ReplaceOneAsync(filtro, livro);
             }
-
-            Console.WriteLine();
-
-            Console.WriteLine("Under The Dome Antes");
-            filtro = Builders<Livro>.Filter.Eq(l => l.Titulo, "Under The Dome");
-            livros = await livrosCollection.Find(filtro).ToListAsync();
-            foreach (var livro in livros)
-            {
-                Console.WriteLine(livro.ToJson<Livro>());
-            }
-            var filtroUpdate = Builders<Livro>.Update.Set(l => l.Ano, 2014);
-            Console.WriteLine("Alterando o Under the Dome");
-            await livrosCollection.UpdateOneAsync(filtro, filtroUpdate);
-            livros = await livrosCollection.Find(filtro).ToListAsync();
-            foreach (var livro in livros)
-            {
-                Console.WriteLine(livro.ToJson<Livro>());
-            }
-
-            Console.WriteLine();
-
-            Console.WriteLine("Alterando os livros do machado de Assis");
-            filtro = Builders<Livro>.Filter.Eq(l => l.Autor, "Machado de Assis");
-            livros = await livrosCollection.Find(filtro).ToListAsync();
-            foreach (var livro in livros)
-            {
-                Console.WriteLine(livro.ToJson<Livro>());
-            }
-            filtroUpdate = Builders<Livro>.Update.Set(l => l.Autor, "M. De Assis");
-            Console.WriteLine("Alterando os livros do machado de Assis");
-            await livrosCollection.UpdateManyAsync(filtro, filtroUpdate);
-            filtro = Builders<Livro>.Filter.Eq(l => l.Autor, "M. De Assis");
-            livros = await livrosCollection.Find(filtro).ToListAsync();
-            foreach (var livro in livros)
-            {
-                Console.WriteLine(livro.ToJson<Livro>());
-            }
-
-            Console.ReadKey();
 
             Console.ReadKey();
         }
